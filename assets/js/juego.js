6da5654d9ed993b7abe915b071d5f70733ba1d28
sr-jerly <jerlymigueldelarosah@gmail.com>
2021-01-10 19:01:41 -0400

    Creacion de todas las funcionalidades del juego

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,7 +1,23 @@
-let deck         = [];
-const tipos      = ['C','D','H','S'];
-let nuevoDeck    = [];
-const especiales = ['A', 'J','Q','K'];
+let deck          = [];
+const tipos       = ['C','D','H','S'];
+let nuevoDeck     = [];
+const especiales  = ['A', 'J','Q','K'];
+let puntosJugador = 0;
+let puntosComputador = 0;
+
+// Variables  para manejo del DOM
+
+const  btnPedirCartas  = document.querySelector('#btnPedirCartas');
+const  btnDetenerJuego = document.querySelector('#btnDetenerJuego');
+const  btnNuevoJuego   = document.querySelector('#btnNuevoJuego');
+
+const cartasJugador    = document.querySelector('#jugador-cartas');
+const cartasComputador = document.querySelector('#conputador-cartas');
+const smallPuntos      = document.querySelectorAll('small');
+
+// console.log( cartasHTML );
+
+
 
 // Funcion ceadora de las cartas 
 const crearDeck = () => {
@@ -15,39 +31,138 @@ const crearDeck = () => {
             deck.push( `${esp}${tipo}` );
         }       
     }
-    for(let i = 0; i < deck.length; i++){
-        control =  Math.round(Math.random() * 51 );
-          ( nuevoDeck.includes(deck[control]) ) ? 
-          i-- : nuevoDeck.push( deck[control] ) ;
-      }
-      return nuevoDeck
+    return deck
 
 }
 
-console.log( crearDeck() );
+crearDeck();
+
+const mezclarCartas = () =>{
+    for(let i = 0; i < deck.length; i++){
+      control =  Math.round(Math.random() * 51 );
+        ( nuevoDeck.includes(deck[control]) ) ? 
+        i-- : nuevoDeck.push( deck[control] ) ;
+    }
+    return nuevoDeck
+}
+
+ mezclarCartas()  
 
 //Funcion para pedir una carta
 
-const pedirCarta = (cartas) => {
+const pedirCarta = () => {
     if(nuevoDeck.length === 0) throw 'Ya no hay mas cartas';
-    let carta = nuevoDeck.pop( [Math.round(Math.random() * cartas.length)] );
-    return carta
+    return nuevoDeck.pop( nuevoDeck );
+    
 }
 
-console.log( pedirCarta( nuevoDeck ) );
+
+//Funcion del valor de la carta
+
+const valorCartas = ( carta ) => {
+    const valor = carta.substring(0, carta.length -1);
+    return (isNaN( valor ))  ? ( valor === 'A' ) ?  11: 10
+                             :parseInt( valor, 10 );
+}
+
+
+valorCartas( pedirCarta());
+
+// Logica del Computador
+
+const turnoComputador = ( puntosMinimos ) => {
+
+    do {
+        const carta =  pedirCarta();
+
+        puntosComputador += valorCartas( carta );
+        smallPuntos[1].innerHTML = puntosComputador;
+    
+        const cartasHTML = document.createElement('img');
+        cartasHTML.src = `assets/cartas/${ carta }.png`;
+        cartasHTML.classList.add('cartas');
+        cartasComputador.append( cartasHTML );
+
+        if(puntosMinimos > 21) break;
+
+    } while ( (puntosComputador < puntosMinimos) && (puntosMinimos <=21) );
+
+ setTimeout(()=>{
+    if( puntosMinimos === puntosComputador ) alert( 'Nadie gana ):' );
+    else if(puntosComputador > 21) alert( 'Has ganado' );
+    else alert( 'El computador gana' );
+ }, 500);
+}
+
+
+
+//Eventos 
+
+//Pedir cartas
+btnPedirCartas.addEventListener('click', ()=>{
+    const carta =  pedirCarta();
+
+    puntosJugador += valorCartas( carta );
+    smallPuntos[0].innerHTML = puntosJugador;
+
+    const cartasHTML = document.createElement('img');
+    cartasHTML.src = `assets/cartas/${ carta }.png`;
+    cartasHTML.classList.add('cartas');
+    cartasJugador.append( cartasHTML );
+
+    if(puntosJugador > 21) {
+        btnPedirCartas.disabled = true;
+        btnDetenerJuego.disabled = true;
+         console.warn('Has Perdido');
+         turnoComputador( puntosJugador );
+    }else if(puntosJugador === 21 ){
+        btnPedirCartas.disabled = true;
+        btnDetenerJuego.disabled = true;
+        console.warn('Has ganado');
+        turnoComputador( puntosJugador );
+    }
+    
+});
+
+//Evento de detener el juego
+
+btnDetenerJuego.addEventListener('click', ()=>{
+    btnPedirCartas.disabled = true;
+    btnDetenerJuego.disabled = true;
+    turnoComputador( puntosJugador );
+  
+   
+});
+
+//Eventoo NuevoJuego
+
+btnNuevoJuego.addEventListener('click', ()=>{
+    console.clear();
+    nuevoDeck =[];
+    puntosJugador    = 0;
+    puntosComputador = 0;
+    smallPuntos[0].innerHTML   = 0;
+    smallPuntos[1].innerHTML   = 0;
+    cartasJugador.innerHTML = '';
+    cartasComputador.innerHTML = '';
+    btnPedirCartas.disabled = false;
+    btnDetenerJuego.disabled = false;
+    mezclarCartas();
+
+
+})
 
 
 
 
-//  Funcion creadora del nuevo arreglo de las cartas mezcladas(Barajadas)
-// const mezclarCartas = () =>{
-//     for(let i = 0; i < deck.length; i++){
-//       control =  Math.round(Math.random() * 51 );
-//         ( nuevoDeck.includes(deck[control]) ) ? 
-//         i-- : nuevoDeck.push( deck[control] ) ;
-//     }
-//     return nuevoDeck
-// }
 
-// console.log(  mezclarCartas()  );
+
+
+
+
+
+
+
+
+
 
